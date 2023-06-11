c201709b7bbf28135cf561206ac94617179f9bb4
steveouyang <284577461@qq.com>
2023-06-11 22:24:07 +0800

    嵌套路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+/* 一级页面 */
 import Dashboard from "@/views/dashboard/Dashboard"
-import Interview from "@/views/interview/Interview"
+import Interview from "@/views/Interview"
 import Library from "@/views/libo/Library"
 import Mine from "@/views/mine/Mine"
+
+/* 二级页面 */
+import Module from "./views/Interview/Module";
+import Question from "@/views/Interview/Question";
+import Blog from "@/views/Interview/Blog";
+import Video from "@/views/Interview/Video";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/interview" element={<Interview />} />
+
+        {/* <Route path="/interview" element={<Interview />} /> */}
+        <Route path='/interview' element={<Interview />}>
+          {/* <Route index element={<Module />} /> */}
+          <Route path='module' element={<Module />} />
+          <Route path='question' element={<Question />} />
+          <Route path='blog' element={<Blog />} />
+          <Route path='video' element={<Video />} />
+        </Route>
+
         <Route path="/library" element={<Library />} />
+
         <Route path="/mine" element={<Mine />} />
       </Routes>
     </Router>
---
+++ src/views/Interview/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom';
+
+export default function () {
+  return (
+    <>
+      <div>Interview</div>
+      
+      {/* Outlet相当于是子路由的占位符 */}
+      <Outlet></Outlet>
+    </>
+  )
+}
