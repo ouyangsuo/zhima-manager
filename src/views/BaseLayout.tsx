206be34562bae0b599ef6beefd95052f330af790
steveouyang <284577461@qq.com>
2023-06-13 23:12:58 +0800

    导航与美化

--- src/views/BaseLayout.tsx
+++ src/views/BaseLayout.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 /* icons */
+import { WindowsFilled } from '@ant-design/icons';
 import { UserOutlined, PieChartOutlined, WindowsOutlined, SendOutlined } from '@ant-design/icons';
 import { SettingFilled, QuestionCircleFilled, YoutubeFilled, SnippetsFilled } from '@ant-design/icons';
 import { AppleFilled, CheckSquareFilled, FireFilled, FolderOpenFilled } from '@ant-design/icons';
@@ -8,10 +9,13 @@ import { GithubFilled, AliwangwangFilled, DiffFilled, HeartFilled, AlipaySquareF
 
 /* antd */
 import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Breadcrumb, Layout, Menu, theme, Button } from 'antd';
 
 /* router */
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+
+/* scss */
+import style from './BaseLayout.module.scss'
 
 const { Header, Content, Sider } = Layout;
 
@@ -86,44 +90,88 @@ const BaseLayout: React.FC = () => {
 
     const navigateTo = useNavigate()
 
+    const [currentMenuGroup, setCurrentMenuGroup] = useState("/library")
+    const [currentMenuItem, setCurrentMenuItem] = useState("/module")
+
+    const location = useLocation();
+    useEffect(() => {
+        console.log('当前页面地址：', location.pathname);
+
+        let start = location.pathname.indexOf("/")
+        let end = location.pathname.lastIndexOf("/")
+        let menuGroup = location.pathname.slice(start, end)
+        let menuItem = location.pathname.slice(end)
+
+        if (currentMenuGroup !== menuGroup) setCurrentMenuGroup(menuGroup)
+        if (currentMenuItem !== menuItem) setCurrentMenuItem(menuItem)
+
+    }, [location]);
+
     return (
 
         <Layout>
-            <Header style={{ display: 'flex', alignItems: 'center' }}>
-                <div className="demo-logo" />
+            <Header className={style.header} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#002b80' }}>
+                <WindowsFilled className={style.logo} />
+                <span className={style.title}>LEKUZHIMA.CLUB</span>
+
+                <div className={style.userInfo}>
+                    <img className={style.avatar} src='https://lekuzhima.club/imgs/avatar.jpeg' />
+                    <Button size="small" className={style.btnLogout} type="primary" ghost>
+                        LOG OUT
+                    </Button>
+                </div>
             </Header>
 
             <Layout>
+
+                {/* 菜单 */}
                 <Sider width={200} style={{ background: colorBgContainer }}>
                     <Menu
                         mode="inline"
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
+                        theme='light'
+                        inlineIndent={12}
+
+                        defaultSelectedKeys={['/dashboard']}
+                        defaultOpenKeys={['/library']}
+                        selectedKeys={[currentMenuItem]}
+                        openKeys={[currentMenuGroup]}
+
                         style={{ height: '100%', borderRight: 0 }}
                         items={items2}
+
                         onClick={({ item, key, keyPath, domEvent }) => {
                             // console.log("item=", item);
                             // console.log("domEvent=", domEvent);
                             console.log("key=", key);
                             console.log("keyPath=", keyPath);
 
+                            setCurrentMenuItem(keyPath[0])
+                            keyPath.length > 1 && setCurrentMenuGroup(keyPath[1])
+
                             const targetRoute = keyPath.reduceRight((pv, cv) => pv + cv)
                             console.log("targetRoute=", targetRoute);
 
                             navigateTo(targetRoute)
                         }}
+
+                        onOpenChange={openKeys => {
+                            console.log("onOpenChange:openKeys=", openKeys);
+                            setCurrentMenuGroup(openKeys[openKeys.length - 1])
+                        }}
                     />
                 </Sider>
 
-                <Layout style={{ padding: '0 24px 24px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                {/* 内容 */}
+                <Layout style={{ padding: '0 10px 16px 10px' }}>
+
+                    <Breadcrumb style={{ margin: '10px 0 10px 0' }}>
+                        <Breadcrumb.Item>{currentMenuGroup.slice(1)}</Breadcrumb.Item>
+                        <Breadcrumb.Item>{currentMenuItem.slice(1)}</Breadcrumb.Item>
                     </Breadcrumb>
+
                     <Content
                         style={{
-                            padding: 24,
+                            padding: 16,
                             margin: 0,
                             minHeight: 280,
                             background: colorBgContainer,
@@ -132,7 +180,9 @@ const BaseLayout: React.FC = () => {
                         <Outlet></Outlet>
                     </Content>
                 </Layout>
+
             </Layout>
+
         </Layout>
 
     );
