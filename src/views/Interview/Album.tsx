17b73b01802b17bceec1c18be6cbb9a090a31b7d
steveouyang <284577461@qq.com>
2023-06-15 09:55:19 +0800

    完成专辑删除

--- src/views/Interview/Album.tsx
+++ src/views/Interview/Album.tsx
@@ -1,6 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { Space, Table, Tag, Pagination } from 'antd';
+import React, { useEffect, useState, createContext } from 'react';
+import { Space, Table, Tag, Pagination, Modal } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
+
+import { ExclamationCircleFilled } from '@ant-design/icons';
+
+const { confirm } = Modal;
+
+const showConfirm = ({ id, name }: { id: number, name: string }, onConfirm: Function) => {
+
+  confirm({
+    title: '确认删除专辑吗?',
+    icon: <ExclamationCircleFilled />,
+    content: JSON.stringify({ id, name }),
+    onOk() {
+      console.log('OK');
+      onConfirm()
+    },
+    onCancel() {
+      console.log('Cancel');
+    },
+  });
+
+};
 
 interface DataType {
   id: number;
@@ -9,14 +30,6 @@ interface DataType {
   questions: number;
   company: string
 }
-
-type TablePaginationPosition =
-  | 'topLeft'
-  | 'topCenter'
-  | 'topRight'
-  | 'bottomLeft'
-  | 'bottomCenter'
-  | 'bottomRight';
 
 const columns: ColumnsType<DataType> = [
   {
@@ -54,22 +67,24 @@ const columns: ColumnsType<DataType> = [
       <Space size="middle">
         <a>详情</a>
         <a>编辑</a>
-        <a>删除</a>
+        <a onClick={e => {
+          console.log("delete", record);
+
+          // 弹窗
+          showConfirm(record, () => {
+            let index = _data.findIndex(item => item === record)
+            _data.splice(index, 1)
+            console.log("已删除", index);
+            comSetData([..._data])
+          })
+
+        }}>删除</a>
       </Space>
     ),
   },
 ];
 
-/* 
-  {
-    id: '1',
-    name: '滴滴八股文精选',
-    img: "https://lekuzhima.club/imgs/dachang-bg.jpg",
-    questions: 10,
-    company: '滴滴',
-  },
-*/
-const data: DataType[] = new Array(25).fill(null).map(
+const _data: DataType[] = new Array(25).fill(null).map(
   (item, index) => ({
     id: index + 1,
     name: '滴滴八股文精选',
@@ -78,20 +93,30 @@ const data: DataType[] = new Array(25).fill(null).map(
     company: '滴滴',
   })
 )
-console.log(data);
-
+// console.log(data);
 
 const pageSize = 5
 
+let comSetData;
 const App: React.FC = () => {
   // const [bottom, setBottom] = useState<TablePaginationPosition>('bottomRight');
+  const [data, setData] = useState<DataType[]>([])
   const [page, setPage] = useState(1)
   const [pageData, setPageData] = useState<DataType[]>([])
+
+  useEffect(() => {
+    setData(_data)
+    comSetData = setData
+  }, [])
 
   useEffect(() => {
     setPageData(data.slice(pageSize * (page - 1), pageSize * page))
   }, [page])
 
+  useEffect(() => {
+    console.log("set new page data");
+    setPageData(data.slice(pageSize * (page - 1), pageSize * page))
+  }, [data.length])
 
   return (
     <div style={{ height: "100%", display: 'flex', flexDirection: 'column', justifyContent: "space-between" }}>
