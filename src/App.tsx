072a60d14c00e9f7a877405f126744f00581a952
steveouyang <284577461@qq.com>
2023-07-13 13:48:32 +0800

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count 1342 is {count}
+          count 1348 is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
